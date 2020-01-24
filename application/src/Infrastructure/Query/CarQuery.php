1bf13418b6f6e44b6ea8967baeca976a55ced19b
Łukasz Matusik <lukasz.matusik@schibsted.com>
2020-01-24 12:48:25 +0100

    Working vcar list

--- application/src/Infrastructure/Query/CarQuery.php
+++ application/src/Infrastructure/Query/CarQuery.php
@@ -4,11 +4,19 @@ declare( strict_types=1 );
 namespace App\Infrastructure\Query;
 
 use App\Application\Query\CarQueryInterface;
+use Doctrine\DBAL\Connection;
 
 final class CarQuery implements CarQueryInterface
 {
+    private Connection $connection;
+
+    public function __construct( Connection $connection )
+    {
+        $this->connection = $connection;
+    }
+
     public function fetchByUserId( int $userId ): object
     {
-        // TODO: Implement fetchByUserId() method.
+        return $this->connection->query('SELECT * FROM car WHERE id = 1');
     }
 }
